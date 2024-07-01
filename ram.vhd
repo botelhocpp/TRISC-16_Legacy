57b1a4b89ffa785d7b4da0be60ac2c2feba2e65d
boltragons <pedrobotelho15@alu.ufc.br>
2024-07-01 12:27:17 -0300

    Added ROM and RAM file support

--- ram.vhd
+++ ram.vhd
@@ -13,6 +13,7 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
+USE STD.TEXTIO.ALL;
 
 ENTITY ram IS
     GENERIC(
@@ -32,22 +33,38 @@ ARCHITECTURE hardware OF ram IS
     SUBTYPE word_t IS STD_LOGIC_VECTOR (N - 1 DOWNTO 0);
     
     TYPE ram_array_t IS ARRAY (0 TO Q - 1) OF word_t;
-    SIGNAL ram_contents : ram_array_t := (
-        x"FFF0",    -- 0 SP
-        x"0020",    -- 2 A
-        x"0040",    -- 4 B
-        x"0061",    -- 6 C
-        x"0000",    -- 8 D
-        OTHERS => x"0000"
-    );
+    
+    -- Load the Data Memory from File
+    FUNCTION InitRAM(file_name : string) RETURN ram_array_t IS
+      FILE text_file : text OPEN read_mode IS file_name;
+      VARIABLE text_line : line;
+      VARIABLE ram_content : ram_array_t;
+      VARIABLE i : INTEGER := 0;
+    BEGIN
+      WHILE NOT ENDFILE(text_file) LOOP
+        readline(text_file, text_line);
+        hread(text_line, ram_content(i));
+        i := i + 1;
+      END LOOP;
+      
+      FOR j IN i TO Q - 1 LOOP
+        ram_content(j) := (OTHERS => '0');
+      END LOOP;
+      
+      RETURN ram_content;
+    END FUNCTION;
+    
+    SIGNAL ram_contents : ram_array_t := InitRAM("data.txt");
 BEGIN    
-    PROCESS(clk)
+    PROCESS(clk, we)
     BEGIN
         IF(RISING_EDGE(clk)) THEN
             IF(we = '1') THEN
                 ram_contents(TO_INTEGER(UNSIGNED(addr(N - 1 DOWNTO 1)))) <= din;
+                dout <= (OTHERS => 'Z');
+            ELSE
+                dout <= ram_contents(TO_INTEGER(UNSIGNED(addr(N - 1 DOWNTO 1))));
             END IF;
-            dout <= ram_contents(TO_INTEGER(UNSIGNED(addr(N - 1 DOWNTO 1))));
         END IF;
     END PROCESS;
 END hardware;
--- rom.vhd
+++ rom.vhd
@@ -14,6 +14,7 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
+USE STD.TEXTIO.ALL;
 
 ENTITY rom IS
     GENERIC(
@@ -31,59 +32,29 @@ ARCHITECTURE hardware OF rom IS
     SUBTYPE word_t IS STD_LOGIC_VECTOR (N - 1 DOWNTO 0);
     
     TYPE rom_array_t IS ARRAY (0 TO Q - 1) OF word_t;
-    CONSTANT rom_contents : rom_array_t := (
-        -- LOAD SP
-        "0001100000000000",     -- 00: MOV R0, #0
-        "0011011100000000",     -- 02: LDR R7, [R0]
-        
-        -- LOAD VAR(A)
-        "0001100000000010",     -- 04: MOV R0, #2
-        "0011000100000000",     -- 06: LDR R1, [R0]
-        
-        -- LOAD VAR(B)
-        "0001100000000100",     -- 08: MOV R0, #4
-        "0011001000000000",     -- 0A: LDR R2, [R0]
-        
-        -- LOAD VAR(C)
-        "0001100000000110",     -- 0C: MOV R0, #6
-        "0011001100000000",     -- 0E: LDR R3, [R0]
-        
-        -- X = A + B
-        "0100010000101000",     -- 10: ADD R4, R1, R2
-        
-        -- PUSH X TO STACK
-        "0000000000010001",     -- 12: PUSH R4
-        
-        -- IF X < C...
-        "0000000010001111",     -- 14: CMP R4, R3
-        "0000100001000010",     -- 16: JLT #16
-        
-        -- ELSE Y = X - C
-        "0101010110001100",     -- 18: SUB R5, R4, R3 
-        
-        -- VAR(D) = Y
-        "0001100000001000",     -- 1A: MOV R0, #8
-        "0010000000010100",     -- 1C: STR [R0], R5
-        
-        -- POP STACK TO Y
-        "0000010100000010",     -- 1E: POP R5
-        
-        -- IF X = Y...
-        "0000000010010111",     -- 20: CMP R4, R5
-        "0000100000001001",     -- 22: JEQ #2
-        
-        -- ELSE
-        "0000111101100000",     -- 24: JMP #-38
-        
-        -- ... END
-        "1111111111111111",     -- 26: HALT
-        
-        -- ... Y = X * C
-        "0110010110001100",     -- 28: MUL R5, R4, R3
-        "0000111110111000",     -- 2A: JMP #-18
-
-        OTHERS => x"0000"
-    );
-BEGIN    
+                                     
+    -- Load the Program Memory from File
+    FUNCTION InitROM(file_name : string) RETURN rom_array_t IS
+      FILE text_file : text OPEN read_mode IS file_name;
+      VARIABLE text_line : line;
+      VARIABLE rom_content : rom_array_t;
+      VARIABLE i : INTEGER := 0;
+    BEGIN
+      WHILE NOT ENDFILE(text_file) LOOP
+        readline(text_file, text_line);
+        bread(text_line, rom_content(i));
+        i := i + 1;
+      END LOOP;
+      
+      FOR j IN i TO Q - 1 LOOP
+        rom_content(j) := (OTHERS => '0');
+      END LOOP;
+      
+      RETURN rom_content;
+    END FUNCTION;    
+    
+    SIGNAL rom_contents : rom_array_t := InitROM("program.txt");
+    
+BEGIN 
     dout <= rom_contents(TO_INTEGER(UNSIGNED(addr(N - 1 DOWNTO 1)))) WHEN (en = '1') ELSE (OTHERS => 'Z');
 END hardware;
